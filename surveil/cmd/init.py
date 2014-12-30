2961b7132e997e216216da466576c1e245a1b3e7
aviau <alexandre.viau@savoirfairelinux.com>
2014-12-30 15:59:46 -0500

    Added surveil-pack-upload cmd
    Change-Id: I0883b9520f42f7f1b3a8ce2ec5dfe918a54ad17d

--- surveil/cmd/init.py
+++ surveil/cmd/init.py
@@ -14,13 +14,21 @@
 
 """Script to initialize Surveil dependencies."""
 
+import os
+# from keystoneclient.v2_0 import client as keystone_client
+# from keystoneclient import session
+# from novaclient.v1_1 import client as nova_client
+
 from surveil.api import config
 
 
 def main():
     # Create a basic config in mongodb
     mongo = config.app_hooks[0].mongo_connection
+
+    # Drop the current shinken config
     mongo.drop_database('shinken')
+
     mongo_shinken = mongo.shinken
     mongo_hosts = mongo_shinken.hosts
     mongo_services = mongo_shinken.services
@@ -38,3 +46,36 @@ def main():
          "notification_period": "24x7", "retry_interval": "3",
          "service_description": "check-surveil-api"}
     )
+
+'''
+    # Make the inventory of the OpenStack install
+    keystone = keystone_client.Client(
+        username=os.environ['OS_USERNAME'],
+        tenant_name=os.environ['OS_TENANT_NAME'],
+        password=os.environ['OS_PASSWORD'],
+        auth_url=os.environ['OS_AUTH_URL'],
+    )
+    endpoints = keystone.service_catalog.get_endpoints()
+    sess = session.Session(auth=keystone)
+
+    # Compute
+    if 'compute' in endpoints.keys():
+        nova_urls = [endpoint['publicURL'] for endpoint in endpoints['compute']]
+        # TODO: add compute hosts to surveil
+
+    # Image
+    if 'image' in endpoints.keys():
+        image_urls = [endpoint['publicURL'] for endpoint in endpoints['image']]
+        # TODO: add image hosts to surveil
+
+    # volume
+    if 'volume' in endpoints.keys():
+        volume_urls = [endpoint['publicURL'] for endpoint in endpoints['volume']]
+        # TODO: add image hosts to surveil
+
+    # identity
+    if 'identity' in endpoints.keys():
+        identity_urls = [endpoint['publicURL'] for endpoint in endpoints['identity']]
+        # TODO: add identity hosts to surveil
+
+'''
