beff438af978dd734f845bce9913755e2084a87d
aviau <alexandre.viau@savoirfairelinux.com>
2015-02-13 15:43:47 -0500

    Containers: separated shinken from surveil
    Change-Id: I4e54d1a64f8a368e1b2328423a0a86f4455d1161

--- surveil/cmd/init.py
+++ surveil/cmd/init.py
@@ -15,6 +15,7 @@
 """Script to reinitialize surveil."""
 
 from surveil.api import config
+import surveilclient.client as sc
 
 
 def main():
@@ -41,3 +42,7 @@ def main():
          "notification_period": "24x7", "retry_interval": "3",
          "service_description": "check-surveil-api"}
     )
+
+    # Reload the surveil config
+    cli_surveil = sc.Client('http://localhost:8080/v1')
+    cli_surveil.reload_config()
