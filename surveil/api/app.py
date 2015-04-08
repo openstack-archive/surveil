d181a29c9834d185ba32b7fa01126a460db56207
Frédéric Vachon <fredvac@gmail.com>
2015-04-08 11:54:05 -0400

    Keystone authentication
    Change-Id: Ib5e062fdedefda2352d81daa5af2a6f2fca44ca0

--- surveil/api/app.py
+++ surveil/api/app.py
@@ -11,18 +11,158 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+import subprocess
+import sys
+import time
+import os
+import inspect
 
+from wsgiref import simple_server
 import pecan
 
+from oslo_config import cfg
+from paste import deploy
 
-def setup_app(config):
+CONF = cfg.CONF
 
-    app_conf = dict(config.app)
+OPTS = [
+    cfg.StrOpt('api_paste_config',
+               default="api_paste.ini",
+               help="Configuration file for WSGI definition of API."
+               ),
+]
+
+CONF.register_opts(OPTS)
+
+
+def get_pecan_config():
+    # Set up the pecan configuration
+    return pecan.configuration.conf_from_file(get_config_filename())
+
+
+def get_config_filename():
+    path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    filename = "config.py"
+    return os.path.join(path, filename)
+
+
+def setup_app(pecan_config):
+    app_conf = dict(pecan_config.app)
 
     app = pecan.make_app(
         app_conf.pop('root'),
-        logging=getattr(config, 'logging', {}),
+        logging=getattr(pecan_config, 'logging', {}),
         **app_conf
     )
 
     return app
+
+
+def build_server():
+    srv = PecanServer()
+    srv.run(get_pecan_config(), get_config_filename())
+
+
+def load_app():
+    return deploy.loadapp('config:/etc/surveil/api_paste.ini')
+
+
+def app_factory(global_config, **local_conf):
+    return VersionSelectorApplication()
+
+
+class VersionSelectorApplication(object):
+    def __init__(self):
+        pc = get_pecan_config()
+
+        def not_found(environ, start_response):
+            start_response('404 Not Found', [])
+            return []
+
+        self.v1 = setup_app(pecan_config=pc)
+        self.v2 = setup_app(pecan_config=pc)
+
+    def __call__(self, environ, start_response):
+        if environ['PATH_INFO'].startswith('/v1/'):
+            return self.v1(environ, start_response)
+        return self.v2(environ, start_response)
+
+
+class PecanServer:
+
+    def __init__(self):
+        self.config = {}
+        self.config_file = ""
+        self.server_process = None
+
+    def run(self, pecan_config, config_file):
+        self.config = pecan_config
+        self.config_file = config_file
+        self.watch_and_spawn()
+
+    def create_subprocess(self):
+
+        self.server_process = subprocess.Popen(
+            ['pecan', 'serve', '--reload', self.config_file],
+            stdin=sys.stdout, stdout=sys.stdout
+        )
+
+    def watch_and_spawn(self):
+        from watchdog.observers import Observer
+        from watchdog.events import (
+            FileSystemEventHandler, FileSystemMovedEvent, FileModifiedEvent,
+            DirModifiedEvent
+        )
+
+        print('Monitoring for changes...')
+        self.create_subprocess()
+
+        parent = self
+
+        class AggressiveEventHandler(FileSystemEventHandler):
+            def should_reload(self, event):
+                for t in (
+                        FileSystemMovedEvent, FileModifiedEvent, DirModifiedEvent
+                ):
+                    if isinstance(event, t):
+                        return True
+                return False
+
+            def on_modified(self, event):
+                if self.should_reload(event):
+                    parent.server_process.kill()
+                    parent.create_subprocess()
+
+        # Determine a list of file paths to monitor
+        paths = self.paths_to_monitor()
+
+        event_handler = AggressiveEventHandler()
+        for path, recurse in paths:
+            observer = Observer()
+            observer.schedule(
+                event_handler,
+                path=path,
+                recursive=recurse
+            )
+            observer.start()
+
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            pass
+
+    def paths_to_monitor(self):
+        paths = []
+
+        for package_name in getattr(self.config.app, 'modules', []):
+            module = __import__(package_name, fromlist=['app'])
+            if hasattr(module, 'app') and hasattr(module.app, 'setup_app'):
+                paths.append((
+                    os.path.dirname(module.__file__),
+                    True
+                ))
+                break
+
+        paths.append((os.path.dirname(self.config.__file__), False))
+        return paths
--- surveil/cmd/api.py
+++ surveil/cmd/api.py
@@ -4,7 +4,7 @@
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -14,14 +14,8 @@
 
 """Starter script for the Surveil API service."""
 
-import os
-import subprocess
-import sys
-
-from surveil import api
+import surveil.api.app as app
 
 
 def main():
-    filename = os.path.join(os.path.dirname(api.__file__), "config.py")
-    subprocess.Popen(['pecan', 'serve', '--reload', filename],
-                     stdin=sys.stdout, stdout=sys.stdout)
+    app.build_server()
