9278fae578f0b3f6a0449a5563b6ebe92686f15c
aviau <alexandre.viau@savoirfairelinux.com>
2015-04-28 13:41:04 -0400

    Added contacts to config API
    Change-Id: I8a1697e80adc1338109d2cb5c1e7a13a5ec18e50

---
+++ surveil/api/controllers/v2/config/contacts.py
@@ -0,0 +1,62 @@
+# Copyright 2015 - Savoir-Faire Linux inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import pecan
+from pecan import rest
+import wsmeext.pecan as wsme_pecan
+
+from surveil.api.datamodel.config import contact
+from surveil.api.handlers.config import contact_handler
+
+
+class ContactsController(rest.RestController):
+
+    @wsme_pecan.wsexpose([contact.Contact])
+    def get_all(self):
+        """Returns all hosts."""
+        handler = contact_handler.ContactHandler(pecan.request)
+        hosts = handler.get_all()
+        return hosts
+
+    @wsme_pecan.wsexpose(contact.Contact, unicode)
+    def get_one(self, contact_name):
+        """Returns a specific contact."""
+        handler = contact_handler.ContactHandler(pecan.request)
+        contact = handler.get(contact_name)
+        return contact
+
+    @wsme_pecan.wsexpose(body=contact.Contact, status_code=201)
+    def post(self, data):
+        """Create a new contact.
+
+        :param data: a contact within the request body.
+        """
+        handler = contact_handler.ContactHandler(pecan.request)
+        handler.create(data)
+
+    @wsme_pecan.wsexpose(contact.Contact, unicode, status_code=204)
+    def delete(self, contact_name):
+        """Returns a specific contact."""
+        handler = contact_handler.ContactHandler(pecan.request)
+        handler.delete(contact_name)
+
+    @wsme_pecan.wsexpose(contact.Contact,
+                         unicode,
+                         body=contact.Contact,
+                         status_code=204)
+    def put(self, contact_name, contact):
+        """Returns a specific contact."""
+        handler = contact_handler.ContactHandler(pecan.request)
+        handler.update(contact_name, contact)
---
+++ surveil/api/handlers/config/contact_handler.py
@@ -0,0 +1,62 @@
+# Copyright 2014 - Savoir-Faire Linux inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from surveil.api.datamodel.config import contact
+from surveil.api.handlers import handler
+
+
+class ContactHandler(handler.Handler):
+    """Fulfills a request on the contact resource."""
+
+    def get(self, contact_name):
+        """Return a contact."""
+
+        c = self.request.mongo_connection.shinken.contacts.find_one(
+            {"contact_name": contact_name}, {'_id': 0}
+        )
+        return contact.Contact(**c)
+
+    def update(self, contact_name, contact):
+        """Modify an existing contact."""
+        contact_dict = contact.as_dict()
+        if "contact_name" not in contact_dict.keys():
+            contact_dict['contact_name'] = contact_name
+
+        self.request.mongo_connection.shinken.contacts.update(
+            {"contact_name": contact_name},
+            contact_dict
+        )
+
+    def delete(self, contact_name):
+        """Delete existing contact."""
+        self.request.mongo_connection.shinken.contacts.remove(
+            {"contact_name": contact_name}
+        )
+
+    def create(self, contact):
+        """Create a new contact."""
+        self.request.mongo_connection.shinken.contacts.insert(
+            contact.as_dict()
+        )
+
+    def get_all(self):
+        """Return all contacts."""
+        contacts = [c for c
+                    in self.request.mongo_connection.
+                    shinken.contacts.find(
+                    {"register": {"$ne": "0"}},  # Don't return templates
+                    {'_id': 0}
+                    )]
+        contacts = [contact.Contact(**c) for c in contacts]
+        return contacts
