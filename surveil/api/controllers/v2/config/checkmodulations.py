4fc4a918ad28abc913840e5569f9f58ec47553cc
flavien.peyre <flavien.peyre@savoirfairelinux.com>
2015-05-05 17:12:21 -0400

    Config API: add check modulation
    Change-Id: Ic892fee3fb0b3e3d7e6348551172b0837a1315c7

---
+++ surveil/api/controllers/v2/config/checkmodulations.py
@@ -0,0 +1,69 @@
+# Copyright 2015 - Savoir-Faire Linux inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import pecan
+from pecan import rest
+import wsmeext.pecan as wsme_pecan
+
+from surveil.api.datamodel.config import checkmodulation
+from surveil.api.handlers.config import checkmodulation_handler
+from surveil.common import util
+
+
+class CheckModulationsController(rest.RestController):
+
+    @util.policy_enforce(['authenticated'])
+    @wsme_pecan.wsexpose([checkmodulation.CheckModulation])
+    def get_all(self):
+        """Returns all  check modulation."""
+        handler = checkmodulation_handler.CheckModulationHandler(pecan.request)
+        checkmodulations = handler.get_all()
+        return checkmodulations
+
+    @util.policy_enforce(['authenticated'])
+    @wsme_pecan.wsexpose(checkmodulation.CheckModulation, unicode)
+    def get_one(self, checkmodulation_name):
+        """Returns a specific check modulation."""
+        handler = checkmodulation_handler.CheckModulationHandler(pecan.request)
+        checkmodulation = handler.get(checkmodulation_name)
+        return checkmodulation
+
+    @util.policy_enforce(['authenticated'])
+    @wsme_pecan.wsexpose(body=checkmodulation.CheckModulation, status_code=201)
+    def post(self, data):
+        """Create a new check modulation.
+
+        :param data: a check modulation within the request body.
+        """
+        handler = checkmodulation_handler.CheckModulationHandler(pecan.request)
+        handler.create(data)
+
+    @util.policy_enforce(['authenticated'])
+    @wsme_pecan.wsexpose(checkmodulation.CheckModulation,
+                         unicode, status_code=204)
+    def delete(self, checkmodulation_name):
+        """Returns a specific check modulation."""
+        handler = checkmodulation_handler.CheckModulationHandler(pecan.request)
+        handler.delete(checkmodulation_name)
+
+    @util.policy_enforce(['authenticated'])
+    @wsme_pecan.wsexpose(checkmodulation.CheckModulation,
+                         unicode,
+                         body=checkmodulation.CheckModulation,
+                         status_code=204)
+    def put(self, checkmodulation_name, checkmodulation):
+        """Update a specific check modulation."""
+        handler = checkmodulation_handler.CheckModulationHandler(pecan.request)
+        handler.update(checkmodulation_name, checkmodulation)
---
+++ surveil/api/handlers/config/checkmodulation_handler.py
@@ -0,0 +1,57 @@
+# Copyright 2014 - Savoir-Faire Linux inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from surveil.api.datamodel.config import checkmodulation
+from surveil.api.handlers import handler
+
+
+class CheckModulationHandler(handler.Handler):
+    """Fulfills a request on the check modulation resource."""
+
+    def get(self, checkmodulation_name):
+        """Return a check modulation."""
+        c = self.request.mongo_connection.shinken.checkmodulations.find_one(
+            {"checkmodulation_name": checkmodulation_name}
+        )
+        return checkmodulation.CheckModulation(**c)
+
+    def update(self, checkmodulation_name, checkmodulation):
+        """Modify existing check modulation."""
+        checkmodulation_dict = checkmodulation.as_dict()
+        if "checkmodulation_name" not in checkmodulation_dict.keys():
+            checkmodulation_dict['checkmodulation_name'] = checkmodulation_name
+
+        self.request.mongo_connection.shinken.checkmodulations.update(
+            {"checkmodulation_name": checkmodulation_name},
+            checkmodulation_dict
+        )
+
+    def delete(self, checkmodulation_name):
+        """Delete an existing check modulation."""
+        self.request.mongo_connection.shinken.checkmodulations.remove(
+            {"checkmodulation_name": checkmodulation_name}
+        )
+
+    def create(self, data):
+        """Create a new check modulation."""
+        self.request.mongo_connection.shinken.checkmodulations.insert(
+            data.as_dict()
+        )
+
+    def get_all(self):
+        """Return all check modulation."""
+        find = self.request.mongo_connection.shinken.checkmodulations.find()
+        checkmodulations = [c for c in find]
+
+        return [checkmodulation.CheckModulation(**c) for c in checkmodulations]
