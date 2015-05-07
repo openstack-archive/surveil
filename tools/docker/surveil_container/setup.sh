3949c048ba95501360fd71d8556334e0f3122604
aviau <alexandre.viau@savoirfairelinux.com>
2015-05-07 09:05:44 -0400

    setup.sh: Changed sed separator
    Change-Id: I1158b1af66033c99be8160f86ef6f57cfb44e3a1

--- tools/docker/surveil_container/setup.sh
+++ tools/docker/surveil_container/setup.sh
@@ -6,10 +6,10 @@ function setup_surveil_api {
         sed -i "s/pipeline =.*/pipeline = surveil-auth api-server/" /etc/surveil/api_paste.ini
     else
         echo -e "=> Setting up Surveil API for keystone authentication..."
-        sed -i "s/pipeline =.*/pipeline = authtoken api-server/" /etc/surveil/api_paste.ini
-        sed -i "s/auth_uri=.*/auth_uri=${SURVEIL_KEYSTONE_ENDPOINT}/" /etc/surveil/api_paste.ini
-        sed -i "s/admin_user=.*/admin_user=${SURVEIL_KEYSTONE_USERNAME}/" /etc/surveil/api_paste.ini
-        sed -i "s/admin_password=.*/admin_password=${SURVEIL_KEYSTONE_PASSWORD}/" /etc/surveil/api_paste.ini
+        sed -i "s|pipeline =.*|pipeline = authtoken api-server|" /etc/surveil/api_paste.ini
+        sed -i "s|auth_uri=.*|auth_uri=${SURVEIL_KEYSTONE_ENDPOINT}|" /etc/surveil/api_paste.ini
+        sed -i "s|admin_user=.*|admin_user=${SURVEIL_KEYSTONE_USERNAME}|" /etc/surveil/api_paste.ini
+        sed -i "s|admin_password=.*|admin_password=${SURVEIL_KEYSTONE_PASSWORD}|" /etc/surveil/api_paste.ini
     fi
 }
 
