8c5b07791aee1882f82f5a1435e781427e30667b
Philippe Pepos Petitclerc <ppeposp@gmail.com>
2015-05-07 11:46:18 -0400

    Added support for Macromodulation objects
    Change-Id: I037014182e47c91e0a30da6a9b009e5981898f4d

---
+++ surveil/api/controllers/v2/config/macromodulations.py
@@ -0,0 +1,70 @@
+# Copyright 2015 - Savoir-Faire Linux inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import pecan
+from pecan import rest
+import wsmeext.pecan as wsme_pecan
+
+from surveil.api.datamodel.config import macromodulation
+from surveil.api.handlers.config import macromodulation_handler
+from surveil.common import util
+
+
+class MacroModulationController(rest.RestController):
+
+    @util.policy_enforce(['authenticated'])
+    @wsme_pecan.wsexpose([macromodulation.MacroModulation])
+    def get_all(self):
+        """Returns all macro modulation objects."""
+        handler = macromodulation_handler.MacroModulationHandler(pecan.request)
+        modulations = handler.get_all()
+        return modulations
+
+    @util.policy_enforce(['authenticated'])
+    @wsme_pecan.wsexpose(macromodulation.MacroModulation, unicode)
+    def get_one(self, timeperiod_name):
+        """Returns a specific macro modulation."""
+        handler = macromodulation_handler.MacroModulationHandler(pecan.request)
+        modulation = handler.get(timeperiod_name)
+        return modulation
+
+    @util.policy_enforce(['authenticated'])
+    @wsme_pecan.wsexpose(body=macromodulation.MacroModulation, status_code=201)
+    def post(self, data):
+        """Create a new macro modulation object.
+
+        :param data: a macro modulation within the request body.
+        """
+        handler = macromodulation_handler.MacroModulationHandler(pecan.request)
+        handler.create(data)
+
+    @util.policy_enforce(['authenticated'])
+    @wsme_pecan.wsexpose(macromodulation.MacroModulation,
+                         unicode,
+                         status_code=204)
+    def delete(self, modulation_name):
+        """Returns a specific macro modulation."""
+        handler = macromodulation_handler.MacroModulationHandler(pecan.request)
+        handler.delete(modulation_name)
+
+    @util.policy_enforce(['authenticated'])
+    @wsme_pecan.wsexpose(macromodulation.MacroModulation,
+                         unicode,
+                         body=macromodulation.MacroModulation,
+                         status_code=204)
+    def put(self, modulation_name, modulation):
+        """Update a specific macro modulation."""
+        handler = macromodulation_handler.MacroModulationHandler(pecan.request)
+        handler.update(modulation_name, modulation)
