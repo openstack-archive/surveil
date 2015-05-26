f34b56b2f582d8b391549c5e3476fb9b449bcebf
aviau <alexandre.viau@savoirfairelinux.com>
2015-05-26 15:46:08 -0400

    Install mod-ceilometer in Alignak
    Change-Id: I72b07016244827a0dee510740dd89ea130917c65

--- tools/docker/surveil_container/setup.sh
+++ tools/docker/surveil_container/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 function setup_surveil_api {
+
+    echo -e "Setting up mod-ceilometer"
+    sed -i "s|<- SURVEIL_OS_AUTH_URL ->|${SURVEIL_OS_AUTH_URL}|" /etc/shinken/modules/ceilometer.cfg
+    sed -i "s|<- SURVEIL_OS_USERNAME ->|${SURVEIL_OS_USERNAME}|" /etc/shinken/modules/ceilometer.cfg
+    sed -i "s|<- SURVEIL_OS_PASSWORD ->|${SURVEIL_OS_PASSWORD}|" /etc/shinken/modules/ceilometer.cfg
+    sed -i "s|<- SURVEIL_OS_TENANT_NAME ->|${SURVEIL_OS_TENANT_NAME}|" /etc/shinken/modules/ceilometer.cfg
+
     if [ $SURVEIL_AUTH_BACKEND = surveil ] ; then
         echo -e "=> Setting up Surveil API for surveil authentication..."
         sed -i "s/pipeline =.*/pipeline = surveil-auth api-server/" /etc/surveil/api_paste.ini
@@ -9,9 +16,9 @@ function setup_surveil_api {
         sed -i "s|pipeline =.*|pipeline = authtoken api-server|" /etc/surveil/api_paste.ini
         sed -i "s|auth_protocol=.*|auth_protocol=${SURVEIL_KEYSTONE_AUTH_PROTOCOL}|" /etc/surveil/api_paste.ini
         sed -i "s|auth_host=.*|auth_host=${SURVEIL_KEYSTONE_HOST}|" /etc/surveil/api_paste.ini
-        sed -i "s|admin_user=.*|admin_user=${SURVEIL_KEYSTONE_ADMIN_USER}|" /etc/surveil/api_paste.ini
-        sed -i "s|admin_password=.*|admin_password=${SURVEIL_KEYSTONE_ADMIN_PASSWORD}|" /etc/surveil/api_paste.ini
-        sed -i "s|admin_tenant_name=.*|admin_tenant_name=${SURVEIL_KEYSTONE_ADMIN_TENANT_NAME}|" /etc/surveil/api_paste.ini
+        sed -i "s|admin_user=.*|admin_user=${SURVEIL_OS_USERNAME}|" /etc/surveil/api_paste.ini
+        sed -i "s|admin_password=.*|admin_password=${SURVEIL_OS_PASSWORD}|" /etc/surveil/api_paste.ini
+        sed -i "s|admin_tenant_name=.*|admin_tenant_name=${SURVEIL_TENANT_NAME}|" /etc/surveil/api_paste.ini
     fi
 }
 
