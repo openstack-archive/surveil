68fd55b6eadf15d6bacf0af32d782cde5f8de134
aviau <alexandre.viau@savoirfairelinux.com>
2015-05-27 17:45:58 -0400

    Added integration tests
    Change-Id: I33ede0f954fdfafc8c5ad4052ff787aa48e0cce2

---
+++ surveil/tests/integration/test_hello.py
@@ -0,0 +1,26 @@
+# Copyright 2014 - Savoir-Faire Linux inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import requests
+
+from surveil.tests.integration import integration_test
+
+
+class TestIntegrationHello(integration_test.MergedIntegrationTest):
+
+    def test_hello(self):
+        self.assertEqual(
+            requests.get("http://localhost:8080/v2/hello").text,
+            'Hello World!'
+        )
