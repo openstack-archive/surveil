1f1ae88fa0247ba7e880c641e619de37ebee2fbf
flavien peyre <peyre.flavien@gmail.com>
2015-07-02 13:19:27 -0400

    Integration test: update host + metrics name
    Change-Id: I2f369300562882f3db880241cb6463b8fbe6c9ea

--- surveil/tests/integration/backend/docker_backend.py
+++ surveil/tests/integration/backend/docker_backend.py
@@ -4,25 +4,24 @@
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
 import os
 import time
 
 from compose.cli import docker_client
 from compose import config as compose_config
 from compose import project as compose_project
+import docker
 from surveilclient import client as sclient
 
 
 class DockerBackend():
-
     def __init__(self):
         pass
 
@@ -61,7 +60,6 @@ class DockerBackend():
             auth_url='http://localhost:8999/v2/auth',
             version='2_0'
         )
-
         #  Wait until Surveil is available
         now = time.time()
         while True:
@@ -82,6 +80,21 @@ class DockerBackend():
                 time.sleep(10)
             else:
                 raise Exception("Surveil could not start")
+
+    def execute_command(self, commands, container_name):
+
+        dclient = docker.Client()
+
+        if container_name is not None:
+            containers = dclient.containers()
+            for container in containers:
+                count = container.get('Image').count(container_name)
+                if ((count == 1 and container_name != 'surveil') or
+                        (count == 2 and container_name == 'surveil')):
+                    for command in commands:
+                        id = dclient.exec_create(container.get('Id')[:12],
+                                                 command)
+                        dclient.exec_start(id['Id'])
 
     def tearDownClass(self):
         self.project.kill()
--- surveil/tests/integration/test_surveil.py
+++ surveil/tests/integration/test_surveil.py
@@ -65,7 +65,12 @@ class TestSeparatedIntegrationSurveil(
         )
 
     def test_delete_host(self):
-        self.test_create_host()
+        """Delete a host and asserts that is is not monitored by Alignak."""
+        self.get_surveil_client().config.hosts.create(
+            host_name='integrationhosttest',
+            address='127.0.0.1',
+            use='generic-host',
+        )
 
         self.get_surveil_client().config.hosts.delete(
             'integrationhosttest'
@@ -90,15 +95,47 @@ class TestSeparatedIntegrationSurveil(
             )
         )
 
-    def test_passive_check(self):
+    def test_update_host(self):
+        """Update a host and asserts that is is monitored by Alignak."""
         self.get_surveil_client().config.hosts.create(
             host_name='integrationhosttest',
             address='127.0.0.1',
             use='generic-host',
         )
-        self.get_surveil_client().config.commands.create(
-            command_name='check_integrationhosttest',
-            command_line='/usr/lib/monitoring/plugins/sfl/check_example'
+
+        self.get_surveil_client().config.hosts.update(
+            host_name='integrationhosttest',
+            host={'host_name': 'host_name_up',
+                  'address': '127.0.1.1'}
+        )
+
+        self.get_surveil_client().config.reload_config()
+
+        def function():
+            status_host = (self.get_surveil_client().
+                           config.hosts.get(host_name='host_name_up'))
+            self.assertTrue(
+                status_host['host_name'].decode() == 'host_name_up' and
+                status_host['address'].decode() == '127.0.1.1' and
+                status_host['use'].decode() == 'generic-host'
+            )
+
+        self.assertTrue(
+            self.try_for_x_seconds(
+                function,
+                time_to_wait=180,
+                cooldown=10,
+                exception=AssertionError,
+                message="Host is not updated."
+            )
+        )
+
+    def test_passive_check(self):
+        """Test  monitoring a host with passive checks."""
+        self.get_surveil_client().config.hosts.create(
+            host_name='integrationhosttest',
+            address='127.0.0.1',
+            use='generic-host',
         )
         self.get_surveil_client().config.services.create(
             check_command="check_integrationhosttest",
@@ -145,14 +182,28 @@ class TestSeparatedIntegrationSurveil(
         )
 
     def test_custom_plugins(self):
+        """Test documentation tutorial monitoring with your custom plugins."""
+        commands = [
+            "mkdir /usr/lib/monitoring/plugins/custom/",
+            "echo -e '#!/bin/bash\necho " +
+            "DISK $1 OK - free space: / 3326 MB (56%);"
+            " | /=2643MB;5948;5958;0;5968" +
+            "' | sudo tee /usr/lib/monitoring/plugins/"
+            "custom/check_example",
+            'chmod +x /usr/lib/monitoring/plugins/custom/'
+            'check_example'
+        ]
+
+        self.execute_command(commands, 'alignak')
+
         self.get_surveil_client().config.hosts.create(
             host_name='integrationhosttest',
-            address='127.0.0.1',
+            address='integration@sfl.com',
             use='generic-host',
         )
         self.get_surveil_client().config.commands.create(
             command_name='check_integrationhosttest',
-            command_line='/usr/lib/monitoring/plugins/sfl/check_example'
+            command_line='$CUSTOMPLUGINSDIR$/check_example $HOSTADDRESS$'
         )
         self.get_surveil_client().config.services.create(
             check_command="check_integrationhosttest",
@@ -165,8 +216,7 @@ class TestSeparatedIntegrationSurveil(
             notification_interval="30",
             notification_period="24x7",
             retry_interval="3",
-            service_description="check_integrationhosttest",
-            passive_checks_enabled="1"
+            service_description="check_integrationhosttest"
         )
 
         self.get_surveil_client().config.reload_config()
@@ -178,8 +228,8 @@ class TestSeparatedIntegrationSurveil(
                     service['service_description'].decode() ==
                     'check_integrationhosttest' and
                     service['plugin_output'].decode() ==
-                    "DISK OK - free space: / 3326 MB (56%);"
-                    " | /=2643MB;5948;5958;0;5968"
+                    "DISK integration@sfl.com OK - free space:"
+                    " / 3326 MB (56%);| /=2643MB;5948;5958;0;5968"
                     for service in status_services)
             )
 
@@ -190,5 +240,35 @@ class TestSeparatedIntegrationSurveil(
                 cooldown=10,
                 exception=AssertionError,
                 message="Custom Plugins is not used"
+            )
+        )
+
+    def test_list_metrics_name_host(self):
+        """Test if a host with use=generic-host have the good metrics name."""
+        self.get_surveil_client().config.hosts.create(
+            host_name='integrationhosttest',
+            address='127.0.0.1',
+            use='generic-host',
+        )
+
+        self.get_surveil_client().config.reload_config()
+
+        def function():
+            metrics_name_hosts = (
+                self.get_surveil_client().status.hosts.metrics.get(
+                    host_name='integrationhosttest')
+            )
+            self.assertTrue(
+                any(metric_name['metric_name'].decode() == 'rtmin'
+                    for metric_name in metrics_name_hosts)
+            )
+
+        self.assertTrue(
+            self.try_for_x_seconds(
+                function,
+                time_to_wait=360,
+                cooldown=10,
+                exception=AssertionError,
+                message="No metric name for host created"
             )
         )
