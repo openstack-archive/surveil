8541f7ef3a5f17111b36dcb9f9934de2e5661419
aviau <alexandre.viau@savoirfairelinux.com>
2015-07-20 14:10:00 -0400

    Changed comma-separated stringfields to lists
    Change-Id: I8c2ad414d300357a80652e495f4fb7da28ab401c

--- surveil/api/datamodel/config/realm.py
+++ surveil/api/datamodel/config/realm.py
@@ -19,14 +19,15 @@ from surveil.api.datamodel import types
 
 
 class Realm(types.Base):
-    realm_name = wsme.wsattr(wtypes.text, mandatory=True)
-    realm_members = wsme.wsattr(wtypes.text, mandatory=True)
-    default = wsme.wsattr(int, mandatory=True)
+    realm_name = wsme.wsattr(wtypes.text, mandatory=False)
+    realm_members = wsme.wsattr(wtypes.ArrayType(wtypes.text),
+                                mandatory=False)
+    default = wsme.wsattr(int, mandatory=False)
 
     @classmethod
     def sample(cls):
         return cls(
             realm_name='World',
-            realm_members='Europe,America,Asia',
+            realm_members=['Europe', 'America', 'Asia'],
             default=0
         )
