517c522f613530049c84a283bed86ca7f6616eb0
aviau <alexandre@alexandreviau.net>
2015-08-12 14:41:17 -0400

    Implemented search
    Change-Id: Id87d47ecb6a27ce89004561d080d827e6457d7f1

--- surveil/api/datamodel/live_query.py
+++ surveil/api/datamodel/live_query.py
@@ -37,6 +37,8 @@ class LiveQuery(types.Base):
     paging = wsme.wsattr(paging.Paging, mandatory=False)
     "Paging."
 
+    search = wsme.wsattr(wtypes.text, mandatory=False)
+
     @classmethod
     def sample(cls):
         return cls(
@@ -60,5 +62,6 @@ class LiveQuery(types.Base):
                 "defined": {
                     "name": True
                 }
-            })
+            }),
+            fuzzy='web'
         )
